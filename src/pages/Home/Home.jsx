e7ee4121f56f3427929f8f52cdeadc6f9d7f8f4c
George Lungu <george.lungu1896@gmail.com>
2023-11-17 22:25:07 +0200

    finished Categories Component html structure + css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 
 import "./Home.scss"
+import Categories from '../../components/Categories/Categories'
 
 const Home = () => {
   return (
     <div className='home'>
       <Slider />
       <FeaturedProducts type="featured"/>
+      <Categories />
       <FeaturedProducts type="trending"/>
     </div>
   )
